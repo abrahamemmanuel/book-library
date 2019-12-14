483d3d22ea049fd8fcd0ce703294e33e2de0bcd5
Abraham Emmanuel <info@zetamindgroup.com>
2019-12-14 16:53:05 +0100

    refactoring: refactored AuthorsController and AuthorsManagementTest

--- app/Http/Controllers/AuthorsController.php
+++ app/Http/Controllers/AuthorsController.php
@@ -2,14 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Author;
+use Illuminate\Http\Request;
 
 class AuthorsController extends Controller
 {
-    public function store(){
-      Author::create(request()->only([
-        'name', 'dob'
-      ]));
+    public function store()
+    {
+        Author::create($this->validateRequest());
+    }
+
+    private function validateRequest()
+    {
+        return request()->validate([
+          'name' => 'required',
+          'dob' => 'required'
+        ]);
     }
 }
--- tests/Feature/AuthorManagementTest.php
+++ tests/Feature/AuthorManagementTest.php
@@ -2,11 +2,10 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
 use App\Author;
 use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class AuthorManagementTest extends TestCase
 {
@@ -16,15 +15,37 @@ class AuthorManagementTest extends TestCase
      */
     public function an_author_can_be_created()
     {
-       $this->post('/authors', [
-         'name' => 'Emmanuel',
-         'dob' => '05/14/1994'
-       ]);
-       
-       $author = Author::all();
+        $this->post('/authors', $this->data());
 
-       $this->assertCount(1, $author);
-       $this->assertInstanceOf(Carbon::class, $author->first()->dob);
-       $this->assertEquals('1994/14/05', $author->first()->dob->format('Y/d/m'));
+        $author = Author::all();
+
+        $this->assertCount(1, $author);
+        $this->assertInstanceOf(Carbon::class, $author->first()->dob);
+        $this->assertEquals('1994/14/05', $author->first()->dob->format('Y/d/m'));
+    }
+
+    /** @test */
+    public function a_name_is_required()
+    {
+        $response = $this->post('/authors', array_merge($this->data(), ['name' => '']));
+
+        $response->assertSessionHasErrors('name');
+
+    }
+    /** @test */
+    public function a_dob_is_required()
+    {
+        $response = $this->post('/authors', array_merge($this->data(), ['dob' => '']));
+
+        $response->assertSessionHasErrors('dob');
+
+    }
+
+    private function data()
+    {
+        return [
+            'name' => 'Emmanuel',
+            'dob' => '05/14/1994',
+        ];
     }
 }
